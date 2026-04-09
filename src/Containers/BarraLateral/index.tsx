a4823a6c072493c71cf329d8a75434629e873ef0
Matheus Nobre <matheusnsenado6@gmail.com>
2026-04-09 16:08:00 -0300

    adicionando filtragem

--- src/Containers/BarraLateral/index.tsx
+++ src/Containers/BarraLateral/index.tsx
@@ -1,19 +1,52 @@
+import { useDispatch, useSelector } from 'react-redux'
 import FiltroCard from '../../Components/FiltroCard'
 import * as S from './styles'
-const BarraLateral = () => (
-  <S.Aside>
-    <div>
-      <S.Campo type="text" placeholder="Buscar" />
-      <S.Filtros>
-        <FiltroCard contador={3} legenda={'pendentes'} />
-        <FiltroCard contador={3} legenda={'concluídas'} />
-        <FiltroCard contador={3} legenda={'urgentes'} />
-        <FiltroCard contador={3} legenda={'importantes'} />
-        <FiltroCard contador={3} legenda={'normal'} />
-        <FiltroCard ativo contador={3} legenda={'todas'} />
-      </S.Filtros>
-    </div>
-  </S.Aside>
-)
+import { alterarTermo } from '../../store/reducers/filtro'
+import { RootReducer } from '../../store'
+import * as enums from '../../utils/enums/tarefa'
 
+const BarraLateral = () => {
+  const dispatch = useDispatch()
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+  return (
+    <S.Aside>
+      <div>
+        <S.Campo
+          type="text"
+          placeholder="Buscar"
+          value={termo}
+          onChange={(evento) => dispatch(alterarTermo(evento.target.value))}
+        />
+        <S.Filtros>
+          <FiltroCard
+            criterio="status"
+            legenda={'pendentes'}
+            valor={enums.Status.PENDENTE}
+          />
+          <FiltroCard
+            criterio="status"
+            legenda={'concluídas'}
+            valor={enums.Status.CONCLUIDA}
+          />
+          <FiltroCard
+            criterio="prioridade"
+            legenda={'urgentes'}
+            valor={enums.Prioridade.URGENTE}
+          />
+          <FiltroCard
+            criterio="prioridade"
+            legenda="importantes"
+            valor={enums.Prioridade.IMPORTANTE}
+          />
+          <FiltroCard
+            criterio="prioridade"
+            legenda={'normal'}
+            valor={enums.Prioridade.NORMAL}
+          />
+          <FiltroCard criterio="todas" legenda={'todas'} />
+        </S.Filtros>
+      </div>
+    </S.Aside>
+  )
+}
 export default BarraLateral
